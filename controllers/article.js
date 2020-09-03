8b3a5de9e922f4bbea32882c77f43772733bf200
Diego Monje <diegichu@gmail.com>
2020-09-03 10:59:15 +0200

    Primer commit

---
+++ controllers/article.js
@@ -0,0 +1,327 @@
+'use strict'
+var validator = require('validator');
+var fs = require('fs');
+var path = require('path');
+var Article = require('../models/article');
+const { exists } = require('../models/article');
+var controller = {
+
+    datosCurso: (req, res) => {
+        var hola = req.body.hola;
+
+        return res.status(200).send({
+            curso: 'Master Udemy',
+            autor: 'Diego Monje',
+            url: 'www.pornhub.es',
+            hola
+        });
+
+    },
+    test: (req, res) => {
+        return res.status(200).send({
+            message: 'Soy la acción test de mi controlador de articulos'
+        });
+    },
+    save: (req, res) => {
+        //RECOGER PARAMETROS
+        var params = req.body;
+        console.log(params);
+        //VALIDAR DATOS(validator)
+        try {
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_content = !validator.isEmpty(params.content);
+
+        } catch (err) {
+            return res.status(200).send({
+                status: 'error',
+                message: 'Faltan datos por enviar'
+            });
+        }
+        if (validate_title && validate_content) {
+            //CREAR OBJETO
+            var article = new Article();
+
+            //ASIGNAR VALORES
+            article.title = params.title;
+            article.content = params.content;
+            article.image = null;
+            //GUARDAR ARTICULO
+            article.save((err, articleStored) => {
+                if (err || !articleStored) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'El articulo no se ha guardado'
+                    });
+                } else {
+                    return res.status(200).send({
+                        status: 'success',
+                        article: articleStored
+                    });
+                }
+            });
+            //DEVOLVER RESPUESTA
+
+            return res.status(200).send({
+                status: 'success',
+                article
+            });
+        } else {
+            return res.status(200).send({
+                status: 'error',
+                message: 'Los datos no son validos'
+            });
+        }
+
+    },
+    getArticles: (req, res) => {
+
+        var query = Article.find({});
+
+        var last = req.params.last;
+        if (last || last != undefined) {
+            query.limit(5);
+        }
+
+        // Find
+        query.sort('-_id').exec((err, articles) => {
+
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al devolver los articulos !!!'
+                });
+            }
+
+            if (!articles) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay articulos para mostrar !!!'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'success',
+                articles
+            });
+
+        });
+    },
+    getArticle: (req, res) => {
+        //RECOGER ID
+        var articleId = req.params.id;
+        //COMPROBAR QUE EXISTE
+        if (!articleId || articleId == null) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'No existe el articulo!'
+            });
+        }
+        //BUSCAR EL ID
+        Article.findById(articleId, (err, article) => {
+
+            if (err || !article) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No existe el articulo!'
+                });
+            }
+
+            //
+            return res.status(200).send({
+                status: 'success',
+                article
+            });
+
+        })
+        //DEVOLVER JSON
+    },
+    update: (req, res) => {
+        //RECOGER ID ARTICULO POR URL
+        var articleId = req.params.id;
+
+        //RECOGER DATOS POR PUT
+        var params = req.body;
+
+        //VALIDAR DATOS
+        try {
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_content = !validator.isEmpty(params.content);
+        } catch (err) {
+            return res.status(200).send({
+                status: 'error',
+                message: 'Faltan datos por enviar'
+            });
+        }
+
+        if (validate_title && validate_content) {
+            //FIND AND UPDATE
+            Article.findOneAndUpdate({ _id: articleId }, params, { new: true }, (err, articleUpdated) => {
+                if (err) {
+                    return res.status(500).send({
+                        status: 'error',
+                        message: 'Error al actualizar!!'
+                    });
+                }
+                if (!articleUpdated) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'No existe el articulo'
+                    });
+                }
+                return res.status(200).send({
+                    status: 'success',
+                    article: articleUpdated
+                });
+
+            });
+        } else {
+            return res.status(200).send({
+                status: 'error',
+                message: 'La validación no es correcta'
+            });
+        }
+
+    },
+    delete: (req, res) => {
+        //RECOGER ID DEL ARTICULO
+        var articleId = req.params.id;
+        //FIND AND DELETE
+        Article.findOneAndDelete({ _id: articleId }, (err, articleRemoved) => {
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al borrar'
+                });
+            }
+            if (!articleRemoved) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No se ha borrado el articulo, posiblemente no exista'
+                });
+            }
+            return res.status(200).send({
+                status: 'success',
+                article: articleRemoved
+            });
+        });
+    },
+    upload: (req, res) => {
+        // Configurar el modulo connect multiparty router/article.js (hecho)
+
+        // Recoger el fichero de la petición
+        var file_name = 'Imagen no subida...';
+
+        if (!req.files) {
+            return res.status(404).send({
+                status: 'error',
+                message: file_name
+            });
+        }
+
+        // Conseguir nombre y la extensión del archivo
+        var file_path = req.files.file0.path;
+        var file_split = file_path.split('\\');
+
+        // * ADVERTENCIA * EN LINUX O MAC
+        // var file_split = file_path.split('/');
+
+        // Nombre del archivo
+        var file_name = file_split[2];
+
+        // Extensión del fichero
+        var extension_split = file_name.split('\.');
+        var file_ext = extension_split[1];
+
+        // Comprobar la extension, solo imagenes, si es valida borrar el fichero
+        if (file_ext != 'png' && file_ext != 'jpg' && file_ext != 'jpeg' && file_ext != 'gif') {
+
+            // borrar el archivo subido
+            fs.unlink(file_path, (err) => {
+                return res.status(200).send({
+                    status: 'error',
+                    message: 'La extensión de la imagen no es válida !!!'
+                });
+            });
+
+        } else {
+            // Si todo es valido, sacando id de la url
+            var articleId = req.params.id;
+
+            if (articleId) {
+                // Buscar el articulo, asignarle el nombre de la imagen y actualizarlo
+                Article.findOneAndUpdate({ _id: articleId }, { image: file_name }, { new: true }, (err, articleUpdated) => {
+
+                    if (err || !articleUpdated) {
+                        return res.status(200).send({
+                            status: 'error',
+                            message: 'Error al guardar la imagen de articulo !!!'
+                        });
+                    }
+
+                    return res.status(200).send({
+                        status: 'success',
+                        article: articleUpdated
+                    });
+                });
+            } else {
+                return res.status(200).send({
+                    status: 'success',
+                    image: file_name
+                });
+            }
+
+        }
+    }, // end upload file
+    getImage: (req, res) => {
+        var file = req.params.image;
+        var path_file = './upload/articles/' + file;
+
+        fs.exists(path_file, (exists) => {
+            if (exists) {
+                return res.sendFile(path.resolve(path_file));
+            } else {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'La imagen no existe !!!'
+                });
+            }
+        });
+    },
+    search: (req, res) => {
+        // Sacar el string a buscar
+        var searchString = req.params.search;
+
+        // Find or
+        Article.find({
+            "$or": [
+                { "title": { "$regex": searchString, "$options": "i" } },
+                { "content": { "$regex": searchString, "$options": "i" } }
+            ]
+        })
+            .sort([['date', 'descending']])
+            .exec((err, articles) => {
+
+                if (err) {
+                    return res.status(500).send({
+                        status: 'error',
+                        message: 'Error en la petición !!!'
+                    });
+                }
+
+                if (!articles || articles.length <= 0) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'No hay articulos que coincidan con tu busqueda !!!'
+                    });
+                }
+
+                return res.status(200).send({
+                    status: 'success',
+                    articles
+                });
+
+            });
+    }
+};  //END CONTROLLER
+
+module.exports = controller;
